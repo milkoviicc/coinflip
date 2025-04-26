85407df9e470c2dd370b686b4e050ed27976099c
Marko Milković <mmilkovic2006@gmail.com>
2025-04-26 13:24:29 +0200

    3d coin animation

--- src/app/components/CoinAnimation.tsx
+++ src/app/components/CoinAnimation.tsx
@@ -1,7 +1,6 @@
 'use client';
 import React, { useState } from 'react'
 
-import spritesheet from '../../../public/spritesheet.png';
 
 const CoinAnimation = () => {
   const [coinResult, setCoinResult] = useState<string | null>(null);
@@ -17,7 +16,7 @@ const CoinAnimation = () => {
   };
   return (
     <div className='flex flex-col items-center justify-between h-screen'>
-      <div className={`w-36 h-48 bg-no-repeat bg-[46%] ${isAnimating ? 'animate-coin-flip' : null}`} style={{backgroundImage: `url(${spritesheet.src})`, backgroundSize: '1000% 100%',}}></div>
+      <div className={`w-36 h-48 ${isAnimating ? 'animate-coin-flip' : 'coin'}`}></div>
       <div className="flex flex-col justify-end items-center w-full h-fit gap-20">
           {isAnimating ? (<h2 className="text-4xl">Coin is flipping</h2>) : !isAnimating && coinResult ? (<h2 className="text-4xl">Coin landed on: {coinResult}</h2>) : null}
           <button className="px-14 py-2 rounded-full bg-green-700 text-2xl cursor-pointer w-fit" onClick={() => flipCoin()}>Flip the coin</button>
